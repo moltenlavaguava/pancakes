5e5f93dfb4ccba0ec14f47a9fff88b06b91d026f
jacob <121923397+moltenlavaguava@users.noreply.github.com>
2026-04-15 12:43:35 -0600

    dev: added github workflow hopefully

--- build.rs
+++ build.rs
@@ -6,6 +6,15 @@ use std::path::PathBuf;
 
 fn main() {
     // this file was made with ai lol
+
+    let target = std::env::var("TARGET").unwrap_or_default();
+    let profile = std::env::var("PROFILE").unwrap_or_default();
+
+    if target.contains("windows") && profile == "release" {
+        println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
+        println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
+    }
+
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let icon_path = out_dir.join("icon.ico");
 
@@ -26,6 +35,31 @@ fn main() {
 
     if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winres::WindowsResource::new();
+
+        if std::env::var("PROFILE").unwrap() == "release" {
+            res.set_manifest(
+                r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="asInvoker" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+<compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1">
+    <application>
+        <!-- Windows 10 and 11 -->
+        <supportedOS Id="{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}" />
+    </application>
+</compatibility>
+</assembly>
+"#,
+            );
+            // The magic flag for the linker
+            println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
+        }
+
         res.set_icon(icon_path.to_str().unwrap());
         res.compile().unwrap();
     }
